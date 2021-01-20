ece77166c89d84561c8fb4e2ae6e45a2abcd1973
Darshana Gunawardana <darshana4it@gmail.com>
2021-01-20 12:08:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import TextInput from "./TextInput";
+import "./styles.scss";
+
+export default function App() {
+  return (
+    <section>
+      <TextInput label="Username" />
+    </section>
+  );
+}
