3206c091b63a7b61ef4716fc78c2ec8b3be5ced1
Darshana Gunawardana <darshana4it@gmail.com>
2021-01-22 22:57:57 +0530

    Cleaning up props

--- src/TextInput/index.js
+++ src/TextInput/index.js
@@ -1,54 +1,35 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import PropTypes from "prop-types";
 import cx from "classnames";
 import styles from "./TextInput.module.scss";
 
 const component = "text-input";
 
-export function TextInput({ label, className, ...props }) {
+export const TextInput = forwardRef(({ label, className, ...props }, ref) => {
   const properties = { className: cx(styles.input, props.className) };
 
   return (
     <div className={styles.wrapper}>
       <label className={styles.label}>{label}</label>
       <div className={styles.content}>
-        <input
-          ref={props.ref}
-          placeholder={props.placeholder}
-          type={props.type}
-          name={props.name}
-          value={props.value}
-          disabled={props.disabled}
-          id={props.id}
-          {...properties}
-        />
+        <input ref={ref} {...properties} />
         <div className={styles.backdrop}></div>
-        {props.touched && props.error && (
-          <span className="required">{props.error}</span>
-        )}
       </div>
     </div>
   );
-}
+});
 
 TextInput.propTypes = {
   id: PropTypes.string,
   className: PropTypes.string,
-  type: PropTypes.string,
-  placeholder: PropTypes.string,
-  disabled: PropTypes.bool,
-  onChange: PropTypes.func,
-  name: PropTypes.string,
-  value: PropTypes.string,
-  touched: PropTypes.bool,
-  error: PropTypes.string,
+  label: PropTypes.string,
   ref: PropTypes.func
 };
 
 TextInput.defaultProps = {
-  type: "text",
-  disabled: false,
-  touched: false
+  label: "Label"
 };
+
+TextInput.displayName = component;
 
 export default TextInput;
